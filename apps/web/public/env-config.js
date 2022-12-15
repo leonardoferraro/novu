04e52210acffc00862d816f9e1e3d186d4c21c49
leonardoferraro <leonardoferraro@gmail.com>
2022-12-15 15:09:05 +0000

    fix: env

--- apps/web/public/env-config.js
+++ apps/web/public/env-config.js
@@ -1,3 +1,10 @@
 window._env_ = {
-  SKIP_PREFLIGHT_CHECK: 'true',
-};
+  SKIP_PREFLIGHT_CHECK: "true",
+  REACT_APP_API_URL: "https://3000-novuhq-novu-1t1u7e5a1rt.ws-us79.gitpod.io",
+  REACT_APP_WIDGET_SDK_PATH: "sdk_path",
+  REACT_APP_ENVIRONMENT: "dev",
+  GLOBAL_CONTEXT_PATH: "",
+  WEB_CONTEXT_PATH: "",
+  REACT_APP_INTERCOM_APP_ID: "intercom",
+  REACT_APP_WS_URL: "https://3002-novuhq-novu-1t1u7e5a1rt.ws-us79.gitpod.io",
+}
--- apps/widget/public/env-config.js
+++ apps/widget/public/env-config.js
@@ -1,4 +1,10 @@
 window._env_ = {
-  SKIP_PREFLIGHT_CHECK: 'true',
-  dima: 'test',
-};
+  SKIP_PREFLIGHT_CHECK: "true",
+  REACT_APP_API_URL: "https://3000-novuhq-novu-1t1u7e5a1rt.ws-us79.gitpod.io",
+  REACT_APP_WS_URL: "https://3002-novuhq-novu-1t1u7e5a1rt.ws-us79.gitpod.io",
+  GLOBAL_CONTEXT_PATH: "",
+  WIDGET_CONTEXT_PATH: "",
+  WIDGET_CONTEXT_PATH: "",
+  REACT_APP_ENVIRONMENT: "dev",
+  BROWSER: "none",
+}
